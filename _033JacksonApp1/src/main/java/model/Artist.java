cc01c58ecdfbde032249f503c038e3c0820c5e23
Akshita <agarwalakshita2003@gmail.com>
2025-11-30 02:43:27 +0530

    Add _033, JavaObj to JSON
    Simple Java project - Demo to convert JavaObj to JSON using Jackson API

---
+++ _033JacksonApp1/src/main/java/model/Artist.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Artist {
+	
+	private int aId;
+	private String aName;
+	private String aCity;
+	
+	
+	public Artist() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Artist(int aId, String aName, String aCity) {
+		super();
+		this.aId = aId;
+		this.aName = aName;
+		this.aCity = aCity;
+	}
+
+	@Override
+	public String toString() {
+		return "Artist [aId=" + aId + ", aName=" + aName + ", aCity=" + aCity + "]";
+	}
+
+	public int getaId() {
+		return aId;
+	}
+
+	public void setaId(int aId) {
+		this.aId = aId;
+	}
+
+	public String getaName() {
+		return aName;
+	}
+
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+
+	public String getaCity() {
+		return aCity;
+	}
+
+	public void setaCity(String aCity) {
+		this.aCity = aCity;
+	}
+	
+	
+	
+
+}
